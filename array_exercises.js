260b4c0f882f2e2470f8ae82e7dcf32d27b6159c
PrestonPhelan <preston.phelan@gmail.com>
2016-12-14 14:03:10 -0800

    Through bubbleSort

--- array_exercises.js
+++ array_exercises.js
@@ -36,3 +36,63 @@ Array.prototype.myTranspose = function() {
   }
   return finalArray;
 };
+
+
+Array.prototype.myEach = function(callback) {
+  for(let i = 0; i < this.length; i++) {
+    callback(this[i]);
+  }
+};
+
+const printMe = function(item) {
+  console.log(item);
+};
+
+
+Array.prototype.myMap = function(callback) {
+  let newArray = [];
+  this.myEach(el => {
+    newArray.push(callback(el));
+  });
+  return newArray;
+};
+
+const addOne = function(item) {
+  return item + 1;
+};
+
+Array.prototype.myInject = function(callback) {
+  let acc = this[0];
+  this.slice(1, this.length).myEach(el => {
+    acc = callback(acc, el);
+  });
+  return acc;
+};
+
+function multiply(x, y) { return x * y; }
+
+function bubbleSort(array) {
+  let switched = true;
+  while (switched) {
+    switched = false;
+    for (let i = 0; i < array.length-1; i++) {
+      let j = i + 1;
+      let x = array[i];
+      let y = array[j];
+
+      if (x > y) {
+        let temp = x;
+        array[i] = y;
+        array[j] = temp;
+        switched = true;
+      }
+    }
+  }
+}
+
+function substrings(string) {
+  newArray = [];
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i + i; )
+  }
+}
